7c30824e13378fc0e1d5374ecd8ca980ea35ea0d
k3lly003 <ntirukelly@gmail.com>
2023-06-05 17:59:28 +0300

    pushing changes to make notification endpoints

--- controllers/notification.js
+++ controllers/notification.js
@@ -3,15 +3,15 @@ import CreatenotificationModel from "../models/notificationmodel.js";
 const createNotification = async (req, res) => {
   try {
     let data = req.body;
-    const notificationInstance = await CreatenotificationModel({
+    const notificationInstance = new CreatenotificationModel({
       sendername: data.sendername,
       message: data.message,
     });
-    const response = notificationInstance.save();
+    const response = await notificationInstance.save();
     res.status(200).json({
       message: "Notification Created Successfully",
       error: null,
-      data: notificationInstance,
+      data: response,
     });
   } catch (err) {
     res.status(500).json({
@@ -39,34 +39,58 @@ const getNotification = async (req, res) => {
 };
 const updateNotification = async (req, res) => {
   try {
+    const id = req.params.id;
     const data = req.body;
-    const notificationInstance = await CreatenotificationModel.updateOne({
-      _id: data._id,
+    const notificationInstance = await CreatenotificationModel.find({
+      _id: id,
     });
-    res.status(200).json({
-      message: "Notification Successfully updated",
-      error: "null",
-      data: data,
-    });
+    if (notificationInstance.length == 0) {
+      res.status(400).json({
+        message: "Failer to update notification",
+        error: "information not found",
+        data: null,
+      });
+    } else {
+      const response = await CreatenotificationModel.findOneAndUpdate(
+        { _id: id },
+        { $set: data }
+      );
+      res.status(200).json({
+        message: "Notification Updated Successfully",
+        error: null,
+        data: response,
+      });
+    }
   } catch {
     res.status(500).json({
       message: "Failer to update notification",
       error: "Internal server error",
-      data: "null",
+      data: null,
     });
   }
 };
 const deleteNotification = async (req, res) => {
   try {
-    const data = req.body;
-    const notificationInstance = await CreatenotificationModel.deleteOne({
-      _id: data._id,
+    const id = req.params.id;
+    const notificationInstance = await CreatenotificationModel.find({
+      _id: id,
     });
-    res.status(200).json({
-      message: "Notification successfully deleted",
-      error: "null",
-      data: data,
-    });
+    if (notificationInstance.length == 0) {
+      res.status(400).json({
+        message: "Notification failed deleting",
+        error: "information not deleted",
+        data: null,
+      });
+    } else {
+      const response = await CreatenotificationModel.findOneAndDelete({
+        _id: id,
+      });
+      res.status(200).json({
+        message: "Notification deleted Successfully",
+        error: null,
+        data: response,
+      });
+    }
   } catch {
     res.status(500).json({
       message: "Failed to delete notification",
