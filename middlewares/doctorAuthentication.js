d7c26635265b5320492f61f4533e481f3a522faa
robsdagreat <robsrwibutso12@gmail.com>
2023-06-10 19:48:48 +0200

    added some changes

--- middlewares/doctorAuthentication.js
+++ middlewares/doctorAuthentication.js
@@ -1,29 +1,29 @@
-import jwt from 'jsonwebtoken'
-import CreateDoctorModel from "../models/doctormodel.js";
+import jwt from 'jsonwebtoken';
+import CreateDoctorModel from '../models/doctormodel.js';
 
 const authenticateDoctor = async (req, res, next) => {
- 
-
   try {
-   
+    
+    const token = req.headers.authorization;
+
+    if (!token) {
+      return res.status(401).json({ error: 'Authentication failed. Token missing.' });
+    }
+
+  
     const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
     const userId = decoded.id;
 
-    const check= await CreateDoctorModel.findById({_id : userId})
-      if (!check) {
-        return res.status(401).json({ error: 'Authentication failed' });
-      }
+    const check = await CreateDoctorModel.findById({ _id: userId });
 
-      req.user = check;
-      next();
+    if (!check) {
+      return res.status(401).json({ error: 'Authentication failed' });
+    }
 
-  } 
-
-  catch (error) {
+    next();
+  } catch (error) {
     return res.status(401).json({ error: 'Invalid token' });
   }
+};
 
-}
-
-export default authenticateDoctor
-
+export default authenticateDoctor;
