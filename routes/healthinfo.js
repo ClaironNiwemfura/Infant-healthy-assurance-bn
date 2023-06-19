b6debd7357d623cf71ebf27de06b2ffc32bf03b0
Clairon <niwemfurac@gmail.com>
2023-06-19 22:45:35 +0200

    adding twilio feature

--- routes/healthinfo.js
+++ routes/healthinfo.js
@@ -1,6 +1,7 @@
 import express from "express"
 import bodyParser from "body-parser"
 import {CreateHealthInfo,UpdateHealthInfo,getHealthInfo,deleteHealthInfo}from "../controllers/healthinfo.js";
+import createsms from "../controllers/healthinfosms.js";
 
 const router=express.Router();
 router.use(bodyParser.json());
@@ -10,6 +11,6 @@ router.put("/update/:id",UpdateHealthInfo);
 router.get("/get",getHealthInfo);
 router.delete("/delete/:id",deleteHealthInfo);
 
-
+router.post("/createsms",createsms)
 
 export default router;
